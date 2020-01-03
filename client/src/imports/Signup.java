029b8b82015928e1aefacb998b757c7c52569f59
Tharwat Abouhelal <tharwat96@gmail.com>
2020-01-04 01:08:11 +0200

    Initial classes

---
+++ client/src/imports/Signup.java
@@ -0,0 +1,11 @@
+package imports;
+
+public class Signup
+{
+    public static boolean signup(String userName, String name, String email, String password)
+    {
+        Player e;
+        return false;
+    }
+
+}
