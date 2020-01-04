db0fa150ed87a3d3cbc1d0cdae35989e21a8e87b
AymanMagdy <magdia883@gmail.com>
2020-01-04 16:28:43 +0200

    Database file

---
+++ server/DB_tables.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE player (id int(250) not null primary key auto_increment,
+ first_name varchar(250) not null,
+ last_name varchar(250) not null,
+ email varchar(50) not null,
+ image varchar(250),
+ password varchar(250)); 
