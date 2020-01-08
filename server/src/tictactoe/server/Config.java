0e95bc5fedf0c986e28864ce06c41c144c68a505
AymanMagdy <magdia883@gmail.com>
2020-01-08 18:52:53 +0200

    Adding the Game class and setting the coordinates to the game

--- server/src/tictactoe/server/Config.java
+++ server/src/tictactoe/server/Config.java
@@ -7,8 +7,8 @@ package tictactoe.server;
 public abstract class Config {
     public static final int PORT = 5001;
     public static final String DB_USERNAME = "root";
-    public static final String DB_NAME = "tictactoe";
-    public static final String DB_PASSWORD = "Mm123456";
+    public static final String DB_NAME = "java_tie_toe";
+    public static final String DB_PASSWORD = "TUrvPs20";
     
     
 }
