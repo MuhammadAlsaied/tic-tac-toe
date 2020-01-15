0ca8fdb0717f918362cd0277e140695c1bdfb453
kareem.elkasaby1 <kareem.elkasaby1@gmail.com>
2020-01-15 19:05:07 +0200

    Player Class score Aded

--- client/src/tictactoe/client/App.java
+++ client/src/tictactoe/client/App.java
@@ -114,7 +114,7 @@ public class App extends Application {
         primaryStage.setTitle("TIC TAC TOE!");
 
         mainScene = new Scene(screens.get("signup"), 1350, 700);
-        mainScene = new Scene(screens.get("main"), 1350, 700);
+        mainScene = new Scene(screens.get("playerList"), 1350, 700);
         mainScene.getStylesheets().add(getClass().getResource("/css/style.css").toString());
         primaryStage.setScene(mainScene);
         primaryStage.initStyle(StageStyle.UNDECORATED);
