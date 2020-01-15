9d4caae1428db65f6d69edc2c2947948351b87d8
Tharwat Abou-Helal <tharwat96@gmail.com>
2020-01-15 20:34:37 +0200

    add client config to .gitignore

---
+++ client/src/tictactoe/client/Config - template.java
@@ -0,0 +1,11 @@
+package tictactoe.client;
+
+/**
+ *
+ * @author muhammad
+ */
+public class Config {
+
+    public static final String SERVER_IP = "7.7.7.58";    //7.7.7.58  192.168.43.231
+    public static final int PORT = 5001;
+}
